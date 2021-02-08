3dbffc5a52535a6b4e23dea73bb6769e77a835c5
MarwanZ <marwan.manaf@gmail.com>
2021-02-09 00:47:29 +0200

    aaa

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,51 +19,18 @@ Route::get('/', function () {
 return view('landing');
 });
 
-Route::get('/show-number/{id}', function ($id) {
-    return 'Welcome Mr. '.$id;
-}) -> name('a');
-
-Route::get('/show-string/{id?}', function (){
-        return ' Welcome Mr. Anonymous';
-})-> name('b');
-
-
-
-
-Route::namespace('Front')->group(function(){
-
-    //all route only access controller or mothods in folder Front
-    Route::get('Users');
-
-});
-
-Route::group(['namespace'=> 'Admin'],function(){
-    Route::get('second0','SecondController@showString0');
-    Route::get('second1','SecondController@showString1');
-    Route::get('second2','SecondController@showString2');
-    Route::get('second3','SecondController@showString3');
+Route::get('/landing', function(){
+    return view('landing');
 });
 
 Route::get('login', function(){
     return 'Must Be Login To access this route';
 })-> name('login');
-// Route::get('users','UserController@Index');
-
-Route::get('index','Front\UserController@getIndex');
-
-Route::get('/landing', function(){
-    return view('landing');
-});
 
 Route::get('/about', function(){
     return view('about');
 });
 
-Route::resource('news','NewsController');
-Auth::routes();
+Auth::routes(['verify'=> true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
